40a7437e0feabb2ca6e01a3d9cbef1b7c2a7c93f
Anthony Agnone <ada60@pitt.edu>
2018-07-19 01:26:36 -0400

    Edited styling of register. Changed logo, background, padding, margins and color of nav bar.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
-import MediaQuery from 'react-responsive'
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 
-import Nav from './Pages/Interface/Nav'
-import MobileNav from './Pages/Interface/MobileNav'
-import Home from './Pages/Home'
-import Archive from './Pages/Archive'
-import User from './Pages/User'
-import Draft from './Draft/Draft'
-import Register from './Pages/Register'
+import Nav from './Pages/Interface/Nav';
+import MobileNav from './Pages/Interface/MobileNav';
+import Home from './Pages/Home';
+import Archive from './Pages/Archive';
+import User from './Pages/User';
+import Draft from './Draft/Draft';
+import Register from './Pages/Register';
 
 import './App.css';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
     primary: { main: '#07a7af' },
-    secondary: { main: '#c89b3c' },
+    secondary: { main: '#c89b3c' }
   },
   overrides: {
     MuiBottomNavigation: {
-      root: { background: 'linear-gradient(#17252E, #0E171E)' },
+      root: { backgroundColor: 'transparent' }
     },
     MuiBottomNavigationAction: {
       root: {
-        '&$selected': { color: '#c89b3c' },
-      },
+        '&$selected': { color: '#cdb87c' },
+        backgroundColor: 'transparent'
+      }
     },
     MuiPaper: {
-      root: { backgroundColor: 'transparent' },
-    },
-  },
+      root: { backgroundColor: 'transparent' }
+    }
+  }
 });
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <CssBaseline />
         <BrowserRouter>
           <div>
             {/* WEB */}
             <MediaQuery minDeviceWidth={1224}>
               <Nav />
             </MediaQuery>
-            <Route path='/home' component={Home} />
-            <Route path='/archive' component={Archive} />
-            <Route path='/user' component={User} />
-            <Route path='/draft' component={Draft} />
-            <Route path='/register' component={Register} />
-            <Route exact path='/' render={() => (
-              <Redirect to='/register' />
-            )} />
+            <Route path="/home" component={Home} />
+            <Route path="/archive" component={Archive} />
+            <Route path="/user" component={User} />
+            <Route path="/draft" component={Draft} />
+            <Route path="/register" component={Register} />
+            <Route exact path="/" render={() => <Redirect to="/register" />} />
 
             {/* MOBILE */}
             <MediaQuery maxDeviceWidth={1224}>
@@ -61,7 +62,7 @@ class App extends Component {
           </div>
         </BrowserRouter>
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,78 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Stepper from '@material-ui/core/Stepper'
-import Step from '@material-ui/core/Step'
-import StepLabel from '@material-ui/core/StepLabel'
-import StepContent from '@material-ui/core/StepContent'
-import Button from '@material-ui/core/Button'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 import validator from 'validator';
 
-import lolPittLogo from '../../src/assets/banner.png'
-import BackIcon from '@material-ui/icons/ExpandLess'
-import NextIcon from '@material-ui/icons/ExpandMore'
-import RedoIcon from 'mdi-material-ui/RedoVariant'
-import SendIcon from '@material-ui/icons/Send'
+import lolPittLogo from '../../src/assets/lolpittlogo.png';
+import BackIcon from '@material-ui/icons/ExpandLess';
+import NextIcon from '@material-ui/icons/ExpandMore';
+import RedoIcon from 'mdi-material-ui/RedoVariant';
+import SendIcon from '@material-ui/icons/Send';
 
-import './Pages.css'
+import './Pages.css';
 
 class Register extends Component {
   state = {
     activeStep: 0,
     name: '',
     ign: '',
-    email: '',
-  }
+    email: ''
+  };
 
   handleNext = () => {
     this.setState(state => ({
       activeStep: state.activeStep + 1
-    }))
-  }
+    }));
+  };
 
   handleBack = () => {
     this.setState(state => ({
       activeStep: state.activeStep - 1
-    }))
-  }
+    }));
+  };
 
   handleChange = name => event => {
     this.setState({
-      [name]: event.target.value,
-    })
-    if (event.key === 'Enter'){
-      console.log('do validate')
+      [name]: event.target.value
+    });
+    if (event.key === 'Enter') {
+      console.log('do validate');
     }
-  }
+  };
 
   handleReset = () => {
     this.setState(state => ({
       activeStep: 0
-    }))
-  }
+    }));
+  };
 
   handleSubmit = event => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
 
-  render () {
+  render() {
     const steps = [
       'Tell us your name!',
       'What is your summoner name?',
       'What is your email address?'
-    ]
-    const { activeStep } = this.state
+    ];
+    const { activeStep } = this.state;
 
     return (
-      <div className='registerDisplay'>
-        <div className='main'>
-          <img src={ lolPittLogo } alt='lol@Pitt Logo' />
+      <div className="registerDisplay">
+        <div className="main">
+          <img src={lolPittLogo} alt="lol@Pitt Logo" />
         </div>
-        <div className='registrationSteps'>
-          <Stepper activeStep={activeStep} orientation='vertical'>
+        <div className="registrationSteps">
+          <Stepper activeStep={activeStep} orientation="vertical">
             {steps.map((label, index) => {
               switch (index) {
                 case 0:
@@ -83,30 +83,36 @@ class Register extends Component {
                         <Typography>
                           <form onSubmit={this.handleSubmit}>
                             <TextField
-                              label='Name IRL'
-                              placeholder='Michael Santana'
-                              helperText='Your first and last name here'
+                              label="Name IRL"
+                              placeholder="Michael Santana"
+                              helperText="Your first and last name here"
                               value={this.state.name}
                               onChange={this.handleChange('name')}
                             />
                           </form>
                         </Typography>
-                        <div className='actionsContainer'>
-                          <Button className='disappear' disabled>
+                        <div className="actionsContainer">
+                          <Button className="disappear" disabled>
                             <BackIcon /> Back
                           </Button>
                           <Button
-                            variant='contained'
-                            color='primary'
+                            variant="contained"
+                            color="primary"
                             onClick={this.handleNext}
-                            disabled={this.state.name === '' || !validator.contains(this.state.name, )}
+                            disabled={
+                              this.state.name === '' ||
+                              !validator.contains(this.state.name)
+                            }
                           >
-                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'} <NextIcon />
+                            {activeStep === steps.length - 1
+                              ? 'Finish'
+                              : 'Next'}{' '}
+                            <NextIcon />
                           </Button>
                         </div>
                       </StepContent>
                     </Step>
-                  )
+                  );
                 case 1:
                   return (
                     <Step key={label}>
@@ -115,33 +121,36 @@ class Register extends Component {
                         <Typography>
                           <form onSubmit={this.handleSubmit}>
                             <TextField
-                              label='Summoner Name'
-                              placeholder='Imaqtpie'
-                              helperText='IGN'
+                              label="Summoner Name"
+                              placeholder="Imaqtpie"
+                              helperText="IGN"
                               value={this.state.ign}
                               onChange={this.handleChange('ign')}
                             />
                           </form>
                         </Typography>
-                        <div className='actionsContainer'>
-                          <Button
-                            color='secondary'
-                            onClick={this.handleBack}
-                          >
+                        <div className="actionsContainer">
+                          <Button color="secondary" onClick={this.handleBack}>
                             <BackIcon /> Back
                           </Button>
                           <Button
-                            variant='contained'
-                            color='primary'
+                            variant="contained"
+                            color="primary"
                             onClick={this.handleNext}
-                            disabled={this.state.ign === '' || !validator.isAlphanumeric(this.state.ign)}
+                            disabled={
+                              this.state.ign === '' ||
+                              !validator.isAlphanumeric(this.state.ign)
+                            }
                           >
-                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'} <NextIcon />
+                            {activeStep === steps.length - 1
+                              ? 'Finish'
+                              : 'Next'}{' '}
+                            <NextIcon />
                           </Button>
                         </div>
                       </StepContent>
                     </Step>
-                  )
+                  );
                 case 2:
                   return (
                     <Step key={label}>
@@ -149,57 +158,74 @@ class Register extends Component {
                       <StepContent>
                         <Typography>
                           <form onSubmit={this.handleSubmit}>
-                            <div>This email will be used to confirm your registration!</div>
+                            <div>
+                              This email will be used to confirm your
+                              registration!
+                            </div>
                             <TextField
-                              label='Email Address'
-                              placeholder='Imaqtpielol@gmail.com'
-                              helperText='Preferred Email'
+                              label="Email Address"
+                              placeholder="Imaqtpielol@gmail.com"
+                              helperText="Preferred Email"
                               value={this.state.email}
                               onChange={this.handleChange('email')}
                             />
                           </form>
                         </Typography>
-                        <div className='actionsContainer'>
-                          <Button
-                            color='secondary'
-                            onClick={this.handleBack}
-                          >
+                        <div className="actionsContainer">
+                          <Button color="secondary" onClick={this.handleBack}>
                             <BackIcon /> Back
                           </Button>
                           <Button
-                            variant='contained'
-                            color='primary'
+                            variant="contained"
+                            color="primary"
                             onClick={this.handleNext}
-                            disabled={this.state.email === '' || !validator.isEmail(this.state.email)}
+                            disabled={
+                              this.state.email === '' ||
+                              !validator.isEmail(this.state.email)
+                            }
                           >
-                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'} <NextIcon />
+                            {activeStep === steps.length - 1
+                              ? 'Finish'
+                              : 'Next'}{' '}
+                            <NextIcon />
                           </Button>
                         </div>
                       </StepContent>
                     </Step>
-                  )
+                  );
                 default:
-                  return 'Something went wrong with your registration D:'
+                  return 'Something went wrong with your registration D:';
               }
             })}
           </Stepper>
         </div>
         {activeStep === steps.length && (
-          <Paper square elevation={0} className='CompletionDisplay'>
+          <Paper square elevation={0} className="CompletionDisplay">
             <div>
-              <Typography>Successfully completed - Click to send your confirmation email!</Typography>
-              <Button className='reviewInfo' onClick={this.handleReset} color='secondary'>
-                <RedoIcon className='redoIcon' /> Review Information
+              <Typography>
+                Successfully completed - Click to send your confirmation email!
+              </Typography>
+              <Button
+                className="reviewInfo"
+                onClick={this.handleReset}
+                color="secondary"
+              >
+                <RedoIcon className="redoIcon" /> Review Information
               </Button>
-              <Button className='sendEmail' onClick={this.handleReset} color='primary' variant='extendedFab'>
-                Send Email <SendIcon className='sendIcon' />
+              <Button
+                className="sendEmail"
+                onClick={this.handleReset}
+                color="primary"
+                variant="extendedFab"
+              >
+                Send Email <SendIcon className="sendIcon" />
               </Button>
             </div>
           </Paper>
         )}
       </div>
-    )
+    );
   }
 }
 
-export default Register
+export default Register;

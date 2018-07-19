937d7b5575b216c57b3d18bc0bf4574178372036
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-07-19 18:36:57 -0400

    stream library implemented with an example

--- src/Draft/Draft.js
+++ src/Draft/Draft.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Subscription } from '../streamLib/stream.js'
 
-const Draft = (props) => {
-  return <div>Draft</div>
+class Draft extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {array: []}
+    this.subscription = new Subscription('/draft')
+    this.subscription.on('data', (data) => {
+      this.setState(data)
+    })
+  }
+
+  componentWillUnmount() {
+    this.subscription.end()
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.array.map((a)=> {
+          return (
+            a
+          )
+        })}
+      </div>
+    )
+  }
 }
+
 
 export default Draft
---
+++ src/streamLib/stream.js
@@ -0,0 +1,61 @@
+import http from 'stream-http'
+
+const host = 'https://localhost:4200'
+
+let isFunction = function(obj) {
+    return typeof obj == 'function' || false;
+};
+
+class Subscription {
+  constructor (url) {
+    this.url = host + url
+    this.req = http.get(this.url, (res) => {
+      res.on('data', (buf) => {
+        this.emit('data', JSON.parse(buf))
+      })
+    });
+    this.listeners = new Map();
+  }
+
+  end() {
+    this.req.abort()
+  }
+
+  on(label, cb) {
+    this.listeners.has(label) || this.listeners.set(label, []);
+    this.listeners.get(label).push(cb);
+  }
+
+  removeListener(label, callback) {
+    let listeners = this.listeners.get(label),
+        index;
+
+    if (listeners && listeners.length) {
+        index = listeners.reduce((i, listener, index) => {
+            return (isFunction(listener) && listener === callback) ?
+                i = index :
+                i;
+        }, -1);
+
+        if (index > -1) {
+            listeners.splice(index, 1);
+            this.listeners.set(label, listeners);
+            return true;
+        }
+    }
+    return false;
+  }
+
+  emit(label, ...args) {
+    let listeners = this.listeners.get(label)
+    if (listeners && listeners.length) {
+        listeners.forEach((listener) => {
+            listener(...args);
+        });
+        return true;
+    }
+    return false;
+  }
+}
+
+export { Subscription }

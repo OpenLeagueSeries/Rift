47fd63b9b85fad0c54c3f215bbd72578c3306d7f
bmgarness <bmgarness13@gmail.com>
2018-08-04 23:05:30 -0400

    Revert "Feature/stream library"

--- src/Draft/Draft.js
+++ src/Draft/Draft.js
@@ -1,40 +1,7 @@
-import React, { Component } from 'react'
-import { Subscription } from '../streamLib/stream.js'
+import React from 'react'
 
-class Draft extends Component {
-
-  constructor (props) {
-    super(props)
-    this.state = {value: 0, history: []}
-  }
-
-  componentDidMount() {
-    this.subscription = new Subscription('/draft',
-    (data) => {
-      this.setState({value: data.number, history: [ ...this.state.history, 'server sent: ' + data.number]})
-      if (data.number > 1) {
-        const collatz = (data.number%2 === 1 ? 3 * data.number + 1: data.number/2)
-        this.subscription.request({number: collatz})
-        this.setState({history: [ ...this.state.history, 'client sent: ' + collatz]})
-      }
-    })
-  }
-  componentWillUnmount() {
-    this.subscription && this.subscription.end()
-  }
-
-  render () {
-    return (
-      <div style={{color: 'white'}}>
-        {this.state.value}
-        <input onChange={(ev) => {this.subscription.request({number: Number(ev.target.value)})}}></input>
-        {this.state.history.map((h)=> {
-          return (<p>{h}</p>)
-        } )}
-      </div>
-    )
-  }
+const Draft = (props) => {
+  return <div>Draft</div>
 }
-
 
 export default Draft

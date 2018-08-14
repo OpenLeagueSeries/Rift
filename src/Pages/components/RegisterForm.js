280a21afc2999d295045590588d9e778cc5f9d80
Anthony Agnone <ada60@pitt.edu>
2018-08-14 13:43:52 -0400

    Redesigned registration page. Removed TextField material UI component. Cleaned up scss color hex codes. Fixed spacing on header.

--- src/Pages/components/RegisterForm.js
+++ src/Pages/components/RegisterForm.js
@@ -1,82 +1,22 @@
 import React, { Component, Fragment } from 'react';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
 
 import './assets/styles/RegistrationForm.css';
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 25,
-    backgroundColor: 'white',
-    width: '50vw'
-  },
-  root: {
-    background: 'transparent',
-    display: 'block'
-  },
-  input: {
-    color: '#000',
-    padding: '8px 20px 8px 20px',
-    width: '100%',
-    underline: {
-      borderBottom: '1px solid black'
-    }
-  },
-  form: {
-    textAlign: 'center',
-    width: '50%'
-  },
-  button: {
-    marginBottom: 20
-  }
-};
 
 class RegisterForm extends Component {
   render() {
     const { classes } = this.props;
     return (
       <div className="register-wrap">
-        <div className={classes.container}>
-          <h1>Register Today</h1>
-          <form className={classes.form}>
-            <Fragment>
-              <TextField
-                defaultValue="IRL Name"
-                className={classes.root}
-                InputProps={{
-                  className: classes.input
-                }}
-              />
-              <TextField
-                defaultValue="In Game Name"
-                className={classes.root}
-                InputProps={{
-                  className: classes.input
-                }}
-              />
-              <TextField
-                defaultValue="Email"
-                className={classes.root}
-                InputProps={{
-                  className: classes.input
-                }}
-              />
-              <button className={classes.button}>Register</button>
-            </Fragment>
-          </form>
-        </div>
+        <form>
+          <h1>Register Today!</h1>
+          <input type="text" placeholder="Tell us your name!" />
+          <input type="text" placeholder="What is your summoner name?" />
+          <input type="email" placeholder="What is your email address?" />
+          <button>Register</button>
+        </form>
       </div>
     );
   }
 }
 
-RegisterForm.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(RegisterForm);
+export default RegisterForm;

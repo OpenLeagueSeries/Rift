c291575613b4abe4f0a454e4a9c83d859c40767c
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-27 08:13:39 -0400

    redelete the service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();

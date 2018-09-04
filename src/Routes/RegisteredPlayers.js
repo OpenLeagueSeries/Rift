f98806ea7f556d49de46ffc5602985d16b9838e4
Hyun-Tae Jin <htjmario@gmail.com>
2018-09-04 10:10:24 +0900

    basic table set up

--- src/Routes/RegisteredPlayers.js
+++ src/Routes/RegisteredPlayers.js
@@ -3,10 +3,33 @@ import React, { Component } from 'react'
 import './PlayerList.css'
 
 class RegisteredPlayers extends Component {
+  state = {
+
+  }
+
   render () {
     return (
       <div className='playerListDisplay'>
-
+        <table className='tableContainer'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Player Name</th>
+              <th>Summoner Name</th>
+              <th>Roles</th>
+              <th>Notes</th>
+              <th>Captain</th>
+            </tr>
+          </thead>
+          <tbody>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tbody>
+        </table>
       </div>
     )
   }

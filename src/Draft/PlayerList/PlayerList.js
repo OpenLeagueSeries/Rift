10360c675e7f5cd015d183ccbc92c8bbb9a11ec6
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2018-11-04 19:00:58 -0500

    new style stuff

--- src/Draft/PlayerList/PlayerList.js
+++ src/Draft/PlayerList/PlayerList.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { withStyles } from '@material-ui/core/styles'
 
-// import './DraftPlayerList.css'
+import './DraftPlayerList.css'
 
 const PlayerList = (props) => {
 

02104cb6903d79f54be8d9556446d3c3fccfd1fe
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2018-11-04 19:44:33 -0500

    added some styling

--- src/Draft/PlayerList/PlayerList.js
+++ src/Draft/PlayerList/PlayerList.js
@@ -12,29 +12,32 @@ const PlayerList = (props) => {
     <div className='playerList'>
       {props.players.map((player) => {
         return (
-          <div className='player' key={player.ign}>
-            <Card style = {{margin: '10px', width: '40vw', padding: '0px'}}>
-              <CardContent>
-                <h2 className='ign'>{player.ign}</h2>
-                <div className='notes'>{player.notes}</div>
-                <div className='eloDisplay'>
-                  <span className='eloSolo'> <img src={player.eloSoloShield}/> {player.eloSolo} </span>
-                  <span className='eloFlex'> <img src={player.eloFlexShield}/> {player.eloFlex} </span>
+          <Card style = {{margin: '10px', width: '40vw', padding: '0px'}}>
+            <CardContent>
+              <div className='player' key={player.ign}>
+                <div>
+                  <h2 className='ign'>{player.ign}</h2>
+                  <div className='notes'>{player.notes}</div>
                 </div>
-                <span className='championsList'>
-                  {player.champions.map((champ) =>
-                    <div className='champion' key={champ.name}>
-                      <img className='champIcon' src={champ.icon} />
-                      <span className='champName'>{champ.name}</span>
-                      <div className='champWinrate'>{champ.winrate}</div>
-                      <div className='champKDA'>{champ.kda}</div>
-                    </div>
-                  )}
-                </span>
-              </CardContent>
-            </Card>
-
-          </div>
+                <div>
+                  <span className='eloDisplay'>
+                    <div className='eloSolo'> <h4>Solo</h4> <img src={player.eloSoloShield}/> {player.eloSolo} </div>
+                    <div className='eloFlex'> <h4>Flex</h4> <img src={player.eloFlexShield}/> {player.eloFlex} </div>
+                  </span>
+                  <span className='championsList'>
+                    {player.champions.map((champ) =>
+                      <span className='champion' key={champ.name}>
+                        <img className='champIcon' src={champ.icon} />
+                        <span className='champName'>{champ.name}</span>
+                        <div className='champWinrate'>{champ.winrate}</div>
+                        <div className='champKDA'>{champ.kda}</div>
+                      </span>
+                    )}
+                  </span>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         )
       })}
     </div>

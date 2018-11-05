38855850e556e1bc5355a56db5f4a968d82bf6d0
jaredclin <jaredc0307@gmail.com>
2018-11-04 19:44:03 -0500

    edit roster player

--- src/Draft/RosterPlayer.js
+++ src/Draft/RosterPlayer.js
@@ -10,14 +10,14 @@ const RosterList = ({player: {
   }}) => {
   return (
     <div class="rosterSlot">
-      <span class='elo'> <img src={eloShield}/> {elo} </span>
-      <span class='ign'>{ign}</span>
-      <span class='championsList'>
+      <span className='ign'>{ign}</span>
+      <span className='elo'> <img src={eloShield}/> {elo} </span>
+      <span className='championsList'>
         {champions.map((champ) =>
-          <div class="champion">
+          <div className="champion">
             <img src={champ.icon} />
-            <div>{champ.winrate}</div>
-            <div>{champ.kda}</div>
+            //<div>{champ.winrate}</div>
+            //<div>{champ.kda}</div>
           </div>
         )}
       </span>

5024c624c991d858d56dec8d42013258fd8d802e
Anthony Agnone <37661619+anthonyAgnone@users.noreply.github.com>
2018-12-10 17:40:49 -0500

    Feature/draft view setup (#21)
    * Started laying out draft and small elements of the teams and current bid section
    
    * Finished current player bidding component
    
    * Touch up of padding and margins on current player
    
    * Suggested fixes from pull request

---
+++ src/Draft/PlayerList/CurrentPlayer.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+import Avatar from '@material-ui/core/Avatar'
+
+import goldIV from '../../assets/gold_4.png'
+
+import './current-player.css'
+
+const CurrentPlayer = ({ player }) => {
+  //temporary but set up for player to be a prop
+  player = {
+    name: 'Anthony Agnone',
+    initials: 'AA',
+    ign: 'WindowPane',
+    notes:
+      "Washed up scuffed don't make a captain etc etc etc Washed up scuffed don't make a captain etc etc etc Washed up scuffed don't make a captain etc etc etc. Also as a note to me. I don't like this. This needs a lot of work. But it gets the basic idea done.",
+    elo: 1500,
+    shield: goldIV,
+    role: 'jg / supp',
+    champions: [
+      {
+        name: 'Ivern',
+        wr: '100%',
+        kda: '10'
+      },
+      {
+        name: 'Ivern',
+        wr: '100%',
+        kda: '10'
+      },
+      {
+        name: 'Ivern',
+        wr: '100%',
+        kda: '10'
+      }
+    ]
+  }
+  const championList = player.champions.map(champion => (
+    <champ>
+      <p>{champion.name}</p>
+      <p>w/r: {champion.wr}</p>
+      <p>kda: {champion.kda}</p>
+    </champ>
+  ))
+  return (
+    <div className="current-player-wrapper">
+      <div className="currentBid">
+        <h1>Bid:</h1>
+        <h1>14pts</h1>
+      </div>
+      {/* <Avatar className="avatar">{player.initials}</Avatar> */}
+      <h1>{player.name}</h1>
+      <a href={`http://na.op.gg/summoner/userName=${player.ign}`}>op.gg</a>
+      <h1 className="ign">{player.ign}</h1>
+      <img src={player.shield} alt="" className="shield" />
+      <h2 className="elo">
+        <p>ELO: </p>
+        {player.elo}
+      </h2>
+      <h2 className="role">{player.role}</h2>
+      <h4 className="notes">{player.notes}</h4>
+      <div className="champSection">{championList}</div>
+    </div>
+  )
+}
+
+export default CurrentPlayer
--- src/Draft/TeamList/TeamList.js
+++ src/Draft/TeamList/TeamList.js
@@ -1,31 +1,21 @@
 import React from 'react'
+import Team from './Team'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 
 import RosterPlayer from '../RosterPlayer'
 
-const TeamList = (props) => {
-    return (
-      <div class="TeamList">
-        {props.teams
-          .sort((teamA, teamB) => {
-              return teamB.pointsLeft - teamA.pointsLeft
-            }
-          )
-          .map((team) => {
-            return (
-              <div>
-                <h3 class="teamName">{team.name}</h3>
-                <h5 class="captainName">{team.captainName}</h5>
-                <span>{team.pointsLeft}</span>
-                {team.players.map((p)=> {
-                  return(
-                    <RosterPlayer player={p} />
-                  )
-                })}
-              </div>
-            )
-        })}
-      </div>
-    )
+const TeamList = ({ teams, currentPick }) => {
+  const teamElements = teams.map((team, i) => (
+    <ListItem style={{ padding: 0 }} divider button>
+      <Team team={team} isCurrent={currentPick === i} />
+    </ListItem>
+  ))
+  return (
+    <List component="div" className="teamsList">
+      <div class="TeamList">{teamElements}</div>
+    </List>
+  )
 }
 
 export default TeamList

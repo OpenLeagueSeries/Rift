71532e4a4fc18813755f925cad0d53c877ce01e3
Hyun-Tae Jin <htjmario@gmail.com>
2019-01-23 12:57:56 -0500

    Updates

---
+++ precache-manifest.015aedfc7bb08521b9ba654be8abb6d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "418cb965cccf16e55aeac63dccc9b9b0",
+    "url": "/RiftCloned/static/media/yeah-yeah.418cb965.gif"
+  },
+  {
+    "revision": "5ba977d3d87a3184ac51",
+    "url": "/RiftCloned/static/css/main.cfe43d53.chunk.css"
+  },
+  {
+    "revision": "9d8418d5ed930817955d7a917eb032e7",
+    "url": "/RiftCloned/static/media/TSIcon.9d8418d5.svg"
+  },
+  {
+    "revision": "888d795d7111f2575f53",
+    "url": "/RiftCloned/static/js/1.888d795d.chunk.js"
+  },
+  {
+    "revision": "2076b4d31a04c7248530",
+    "url": "/RiftCloned/static/js/runtime~main.2076b4d3.js"
+  },
+  {
+    "revision": "4e16664d7a799805f826aee2fb9c1157",
+    "url": "/RiftCloned/static/media/newestPittLogo.4e16664d.png"
+  },
+  {
+    "revision": "5ba977d3d87a3184ac51",
+    "url": "/RiftCloned/static/js/main.5ba977d3.chunk.js"
+  },
+  {
+    "revision": "1f5b5128247f5b4ca49bf3b865b4ada7",
+    "url": "/RiftCloned/static/media/cookie.1f5b5128.png"
+  },
+  {
+    "revision": "e84d35b06201b9e4b0451dab7aada160",
+    "url": "/RiftCloned/static/media/gold_4.e84d35b0.png"
+  },
+  {
+    "revision": "67f13f04a0260c2efe07e098c1fec8d6",
+    "url": "/RiftCloned/static/media/TSIconHover.67f13f04.svg"
+  },
+  {
+    "revision": "888d795d7111f2575f53",
+    "url": "/RiftCloned/static/css/1.f434bc4f.chunk.css"
+  },
+  {
+    "revision": "674c78a21f04036a3befebf1ecb80689",
+    "url": "/RiftCloned/index.html"
+  }
+];
